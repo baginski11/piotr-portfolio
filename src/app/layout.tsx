a1d8c7d1b92c6851c02f3578a311b9dd8ad451ec
Maciej <maciejbaginski2008@icloud.com>
2024-10-04 14:31:00 +0200

    Initial commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,15 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import {JetBrains_Mono} from "next/font/google";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+const jetBrainsMono = JetBrains_Mono({
+  subsets: ["latin"],
+  variable: "--font-jetbrains-mono",
+    }
+)
+
+
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -26,7 +24,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${jetBrainsMono.variable} antialiased`}
       >
         {children}
       </body>
